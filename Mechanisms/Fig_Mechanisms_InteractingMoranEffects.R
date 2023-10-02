e49a8772a500a884e3ad05815d3dd747ae18575b
Dan Reuman <reuman@ku.edu>
2023-10-02 14:07:55 -0500

    modified the earier version to make it simpler

--- Mechanisms/Fig_Mechanisms_InteractingMoranEffects.R
+++ Mechanisms/Fig_Mechanisms_InteractingMoranEffects.R
@@ -154,7 +154,7 @@ text(sintro_1_x[2,1]+le1/2,vertoffset-1+1.75,latex2exp::TeX("$l_{e1}$"),cex=0.7,
 text(sinpks_1_x[1,2]+le1/2,1+.5,latex2exp::TeX("$l_{e1}$"),cex=0.7,adj=c(0.5,-.1))
 
 #panel label and title
-mtext(expression(paste("Scenario ",alpha,"1: Synergistic interactions")),side=3,line=0,cex=1,at=xlimits[1],adj=0)
+mtext(expression(paste("Scenario 1: Synergistic interactions")),side=3,line=0,cex=1,at=xlimits[1],adj=0)
 text(xlimits[1],ylimits[2],"(a)",adj=c(0,1))
 
 #label ln
@@ -272,29 +272,17 @@ for (csw in 1:dim(sinpks_2_x)[1])
   }
 }
 
-
-dev.off()
-stop()
-
-
-
 #label the arrows to show the lags
-text(sinpks_2_x[3,2]+le2/2,2*vertoffset+1+.5,latex2exp::TeX("$l_{e2}$"),cex=0.7,adj=c(0.5,-.1))
+#text(sinpks_2_x[3,2]+le2/2,2*vertoffset+1+.5,latex2exp::TeX("$l_{e2}$"),cex=0.7,adj=c(0.5,-.1))
 text(sintro_2_x[2,1]+le2/2,vertoffset-1+2.2,latex2exp::TeX("$l_{e2}$"),cex=0.7,adj=c(0.5,-.1))
 text(sinpks_2_x[1,2]+le2/2,1+.5,latex2exp::TeX("$l_{e2}$"),cex=0.7,adj=c(0.5,-.1))
 
 #panel label
-if (partone)
-{
-  text(xlimits[1],ylimits[2],"(b)",adj=c(0,1))
-} else
-{
-  text(xlimits[1],ylimits[2],"(b)",adj=c(0,1))
-}
+text(xlimits[1],ylimits[2],"(b)",adj=c(0,1))
 
 #label ln
 par(xpd=NA)
-lines(rep(sinpks_2_x[3,1],2),c(2*vertoffset+1+.5,2*vertoffset+1+7.5),type="l",lty="solid",col="violet")
+lines(rep(sinpks_2_x[2,1],2),c(vertoffset+1+.5,vertoffset+1+7.5),type="l",lty="solid",col="violet")
 par(xpd=FALSE)
 
 #***part 2, which shows antagonistic effects, compressed presentation
@@ -306,22 +294,22 @@ period<-20
 phase1<-(-1.5)
 phase2<-phase1+2*pi*ln/period
 
-phase_jitters_1<-2*pi*(2*runif(3)-1)/period
-phase_jitters_2<-2*pi*(3*runif(3)-1)/period
+phase_jitters_1<-2*pi*(2*runif(2)-1)/period
+phase_jitters_2<-2*pi*(3*runif(2)-1)/period
 
 vertoffset<-8
 
-envvar_1<-matrix(NA,3,length(tim))
-envvar_2<-matrix(NA,3,length(tim))
+envvar_1<-matrix(NA,2,length(tim))
+envvar_2<-matrix(NA,2,length(tim))
 for (counter in 1:(dim(envvar_1)[1]))
 {
   envvar_1[counter,]<-sin(2*pi*tim/period+phase1+phase_jitters_1[counter])+(counter-1)*vertoffset
   envvar_2[counter,]<-sin(2*pi*tim/period+phase2+phase_jitters_2[counter])+(counter-1)*vertoffset
 }
 
-sinpks_1_x<-matrix(NA,3,4)
+sinpks_1_x<-matrix(NA,2,4)
 sinpks_1_x[,1]<-(pi/2-phase1-phase_jitters_1)*period/2/pi
-sinpks_2_x<-matrix(NA,3,4)
+sinpks_2_x<-matrix(NA,2,4)
 sinpks_2_x[,1]<-(pi/2-phase2-phase_jitters_2)*period/2/pi
 for (counter in 2:4)
 {
@@ -344,24 +332,21 @@ par(fig=c((yaxwd+panwd+yaxwd)/totwd,
 #do the start of the plotting
 xlimits<-c(0,60) #range(tim)
 ylimits<-range(envvar_1)
-ylimits[2]<-ylimits[2]+.2*diff(ylimits)
+ylimits[2]<-ylimits[2]+.4*diff(ylimits)
 plot(tim,envvar_1[1,],type="n",ylim=ylimits,xlim=xlimits,xaxs="i",xaxt="n",yaxt="n")
 axis(side=1,label=FALSE)
 #put the population rectangles, only done here so other stuff plots over it
 rect(xlimits[1],vertoffset/2-1,xlimits[2],vertoffset/2+1,col="lightgrey",border=NA)
 rect(xlimits[1],3*vertoffset/2-1,xlimits[2],3*vertoffset/2+1,col="lightgrey",border=NA)
-rect(xlimits[1],5*vertoffset/2-1,xlimits[2],5*vertoffset/2+1,col="lightgrey",border=NA)
 #put rectangles to highlight the synchrony, only done here so other stuff plots over it
 ep<-1.2
-rect(mean(sintro_1_x[,2])+le1-ep,-0.5,mean(sintro_1_x[,2])+le1+ep,2*vertoffset+4.5,border=NA,col=rgb(255/255,192/255,203/255,.5)) #"pink" is red    255, green  192, blue   203
-rect(mean(sinpks_1_x[,3])+le1-ep,-0.5,mean(sinpks_1_x[,3])+le1+ep,2*vertoffset+4.5,border=NA,col=rgb(135/255,206/255,250/255,.5)) #"lightskyblue" is red    135, green  206, blue   250
+rect(mean(sintro_1_x[,2])+le1-ep,-0.5,mean(sintro_1_x[,2])+le1+ep,vertoffset+4.5,border=NA,col=rgb(255/255,192/255,203/255,.5)) #"pink" is red    255, green  192, blue   203
+rect(mean(sinpks_1_x[,3])+le1-ep,-0.5,mean(sinpks_1_x[,3])+le1+ep,vertoffset+4.5,border=NA,col=rgb(135/255,206/255,250/255,.5)) #"lightskyblue" is red    135, green  206, blue   250
 #now finish plotting the env vars and proceed
 lines(tim,envvar_1[1,],type="l")
 lines(tim,envvar_1[2,],type="l")
-lines(tim,envvar_1[3,],type="l")
-text(10,vertoffset/2,latex2exp::TeX("population, $w_3(t)"),adj=c(0,.5),cex=0.6)
-text(10,3*vertoffset/2,latex2exp::TeX("population, $w_2(t)"),adj=c(0,.5),cex=0.6)
-text(10,5*vertoffset/2,latex2exp::TeX("population, $w_1(t)"),adj=c(0,.5),cex=0.6)
+text(10,vertoffset/2,latex2exp::TeX("population, $w_2(t)"),adj=c(0,.5),cex=0.6)
+text(10,3*vertoffset/2,latex2exp::TeX("population, $w_1(t)"),adj=c(0,.5),cex=0.6)
 
 #add arrows showing max positive and negative effects on the pops
 for (csw in 1:dim(sinpks_1_x)[1])
@@ -411,31 +396,19 @@ for (csw in 1:dim(sinpks_1_x)[1])
 }
 
 #label the arrows to show the lags
-text(sinpks_1_x[3,2]+le1/4,2*vertoffset+1+.5,latex2exp::TeX("$l_{e1}$"),cex=0.7,adj=c(0.5,-.1))
+#text(sinpks_1_x[3,2]+le1/4,2*vertoffset+1+.5,latex2exp::TeX("$l_{e1}$"),cex=0.7,adj=c(0.5,-.1))
 text(sintro_1_x[2,1]+le1/4,vertoffset-1+1.75,latex2exp::TeX("$l_{e1}$"),cex=0.7,adj=c(0.5,-.1))
 text(sinpks_1_x[1,2]+le1/4,1+.5,latex2exp::TeX("$l_{e1}$"),cex=0.7,adj=c(0.5,-.1))
 
 #panel label and title
-if (partone)
-{
-  mtext(expression(paste("Scenario ",alpha,"2: Antagonistic interactions")),side=3,line=0,cex=1,at=xlimits[1],adj=0)
-  text(xlimits[1],ylimits[2],"(c)",adj=c(0,1))
-}else
-{
-  mtext(expression(paste("Scenario ",beta,"2: Synergistic interactions")),side=3,line=0,cex=1,at=xlimits[1],adj=0)
-  text(xlimits[1],ylimits[2],"(c)",adj=c(0,1))
-}
+mtext(expression(paste("Scenario 2: Antagonistic interactions")),side=3,line=0,cex=1,at=xlimits[1],adj=0)
+text(xlimits[1],ylimits[2],"(c)",adj=c(0,1))
 
 #label ln
 par(xpd=NA)
 lines(rep(sinpks_1_x[1,1],2),c(1,-4.5),type="l",lty="solid",col="violet")
 par(xpd=FALSE)
-mtext(latex2exp::TeX("$l_{n}$"),side=1,line=0,at=mean(c(sinpks_1_x[1,1],sinpks_2_x[3,1])),cex=0.7,adj=c(.5,.5))
-
-#box around the whole thing
-par(fig=c(0.505,0.995,.005,0.995),
-    mai=c(0,0,0,0),mgp=c(3,.15,0),tcl=-.25,new=TRUE)
-plot(1:3,1:3,type="n",xaxt="n",yaxt="n")
+mtext(latex2exp::TeX("$l_{n}$"),side=1,line=0,at=mean(c(sinpks_1_x[1,1],sinpks_2_x[2,1])),cex=0.7,adj=c(.5,.5))
 
 #set up the panel
 par(fig=c((yaxwd+panwd+yaxwd)/totwd,
@@ -447,32 +420,22 @@ par(fig=c((yaxwd+panwd+yaxwd)/totwd,
 #do the start of the plotting
 xlimits<-c(0,60) #range(tim)
 ylimits<-range(envvar_2)
-ylimits[2]<-ylimits[2]+.2*diff(ylimits)
+ylimits[2]<-ylimits[2]+.4*diff(ylimits)
 plot(tim,envvar_2[1,],type="n",ylim=ylimits,xlim=xlimits,lty="dashed",xaxs="i",xaxt="n",yaxt="n")
 axis(side=1,label=TRUE)
 mtext("Time",side=1,line=1.1)
 #put the population rectangles, only done here so other stuff plots over it
 rect(xlimits[1],vertoffset/2-1,xlimits[2],vertoffset/2+1,col="lightgrey",border=NA)
 rect(xlimits[1],3*vertoffset/2-1,xlimits[2],3*vertoffset/2+1,col="lightgrey",border=NA)
-rect(xlimits[1],5*vertoffset/2-1,xlimits[2],5*vertoffset/2+1,col="lightgrey",border=NA)
 #put rectangles to highlight the synchrony, only done here so other stuff plots over it
 ep<-1.2
-if (partone)
-{
-  rect(mean(sintro_2_x[,2])+le2-ep,-0.5,mean(sintro_2_x[,2])+le2+ep,2*vertoffset+4.5,border=NA,col=rgb(255/255,192/255,203/255,.5)) #"pink" is red    255, green  192, blue   203
-  rect(mean(sinpks_2_x[,2])+le2-ep,-0.5,mean(sinpks_2_x[,2])+le2+ep,2*vertoffset+4.5,border=NA,col=rgb(135/255,206/255,250/255,.5)) #"lightskyblue" is red    135, green  206, blue   250
-}else
-{
-  rect(mean(sintro_2_x[,2])+le2-ep,-0.5,mean(sintro_2_x[,2])+le2+ep,2*vertoffset+4.5,border=NA,col=rgb(135/255,206/255,250/255,.5)) #"pink" is red    255, green  192, blue   203
-  rect(mean(sinpks_2_x[,2])+le2-ep,-0.5,mean(sinpks_2_x[,2])+le2+ep,2*vertoffset+4.5,border=NA,col=rgb(255/255,192/255,203/255,.5)) #"lightskyblue" is red    135, green  206, blue   250
-}
+rect(mean(sintro_2_x[,2])+le2-ep,-0.5,mean(sintro_2_x[,2])+le2+ep,vertoffset+4.5,border=NA,col=rgb(255/255,192/255,203/255,.5)) #"pink" is red    255, green  192, blue   203
+rect(mean(sinpks_2_x[,2])+le2-ep,-0.5,mean(sinpks_2_x[,2])+le2+ep,vertoffset+4.5,border=NA,col=rgb(135/255,206/255,250/255,.5)) #"lightskyblue" is red    135, green  206, blue   250
 #now finish plotting the env vars and proceed
 lines(tim,envvar_2[1,],type="l",lty="dashed")
 lines(tim,envvar_2[2,],type="l",lty="dashed")
-lines(tim,envvar_2[3,],type="l",lty="dashed")
-text(10,vertoffset/2,latex2exp::TeX("population, $w_3(t)"),adj=c(0,.5),cex=0.6)
-text(10,3*vertoffset/2,latex2exp::TeX("population, $w_2(t)"),adj=c(0,.5),cex=0.6)
-text(10,5*vertoffset/2,latex2exp::TeX("population, $w_1(t)"),adj=c(0,.5),cex=0.6)
+text(10,vertoffset/2,latex2exp::TeX("population, $w_2(t)"),adj=c(0,.5),cex=0.6)
+text(10,3*vertoffset/2,latex2exp::TeX("population, $w_1(t)"),adj=c(0,.5),cex=0.6)
 
 for (csw in 1:dim(sinpks_2_x)[1])
 {
@@ -555,22 +518,16 @@ for (csw in 1:dim(sinpks_2_x)[1])
 }
 
 #label the arrows to show the lags
-text(sinpks_2_x[3,2]+3*le2/5,2*vertoffset+1+.5,latex2exp::TeX("$l_{e2}$"),cex=0.7,adj=c(0.5,-.1))
+text(sinpks_2_x[2,2]+2*le2/3,vertoffset+1+.5,latex2exp::TeX("$l_{e2}$"),cex=0.7,adj=c(0.5,-.1))
 #text(sintro_2_x[2,3]+le2/2,vertoffset-1+2.2,latex2exp::TeX("$l_{e2}$"),cex=0.7,adj=c(0.5,-.1))
 #text(sinpks_2_x[1,4]+le2/2,1+.5,latex2exp::TeX("$l_{e2}$"),cex=0.7,adj=c(0.5,-.1))
 
 #panel label
-if (partone)
-{
-  text(xlimits[1],ylimits[2],"(d)",adj=c(0,1))
-} else
-{
-  text(xlimits[1],ylimits[2],"(d)",adj=c(0,1))
-}
+text(xlimits[1],ylimits[2],"(d)",adj=c(0,1))
 
 #label ln
 par(xpd=NA)
-lines(rep(sinpks_2_x[3,1],2),c(2*vertoffset+1+.5,2*vertoffset+1+7.5),type="l",lty="solid",col="violet")
+lines(rep(sinpks_2_x[2,1],2),c(vertoffset+1+.5,vertoffset+1+7.5),type="l",lty="solid",col="violet")
 par(xpd=FALSE)
 
 dev.off()
